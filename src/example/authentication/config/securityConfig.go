dbd0c4495adbb6a674f7bb9ef7a5c269c5da2a09
alperen.karabulut <alperen.karabulut@canovate.com>
2022-06-25 18:43:03 +0300

    Test securityConfig changed

--- src/example/authentication/config/securityConfig.go
+++ src/example/authentication/config/securityConfig.go
@@ -9,9 +9,8 @@ type WebSecurityConfig struct {
 
 func (s WebSecurityConfig) Config(security *roleBuilder.RoleConfigurer) {
 	security.
-		AntMatcher("/api2/v1/test").HasAnyRoles("Admin").
-		AntMatcher("/api3/v1/**").DenyAll().
-		AntMatcher("/api/v1/**").PermitAll().
-		AntMatcher("/api4/v1/**").PermitAll().
+		//AntMatcher("/api/v1/**").PermitAll().
+		//AntMatcher("/api2/v1/test").HasAnyRoles("Admin").
+		//AntMatcher("/api3/v1/**").DenyAll().
 		AnyRequest().DenyAll()
 }
